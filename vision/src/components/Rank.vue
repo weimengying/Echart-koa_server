805f4934894fa29e39b4603767c9a52d122e36f2
[weimengying] <[weimengying515@163.com]>
2020-09-16 17:39:35 +0800

    地区销售排行

--- vision/src/components/Rank.vue
+++ vision/src/components/Rank.vue
@@ -9,7 +9,10 @@ export default {
   data() {
     return {
       chartInstance: null,
-      allData: null
+      allData: null,
+      startValue: 0, // 区域缩放的起点值
+      endValue: 9, // 区域缩放的终点值
+      timerId: null // 定时器的标识
     }
   },
   mounted() {
@@ -21,8 +24,23 @@ export default {
   },
   methods: {
     initChart() {
-      this.chartInstance = this.$echarts.init(this.$refs.rank_ref)
+      this.chartInstance = this.$echarts.init(this.$refs.rank_ref, 'chalk')
       const initOption = {
+        title: {
+          text: '▎地区销售排行',
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '40%',
+          left: '5%',
+          right: '5%',
+          bottom: '5%',
+          containLabel: true // 距离是包含坐标轴上的文字
+        },
+        tooltip: {
+          show: true
+        },
         xAxis: {
           type: 'category'
         },
@@ -36,6 +54,14 @@ export default {
         ]
       }
       this.chartInstance.setOption(initOption)
+      this.chartInstance.on('mouseover', () => {
+        // 鼠标进入的时候停止动画
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        // 鼠标进入的时候开始动画
+        this.startInterval()
+      })
     },
     async getData() {
       // 获取服务器的数据，对this.allData进行赋值之后，调用updateChart方法更新图表
@@ -47,8 +73,14 @@ export default {
       })
       console.log(this.allData)
       this.updateChart()
+      this.startInterval()
     },
     updateChart() {
+      const colorArr = [
+        ['#0BA82C', '#4FF778'],
+        ['#2E72BF', '#23E5E5'],
+        ['#5052EE', '#AB6EE5']
+      ]
       // 处理图表需要的数据
       // 所有省份所形成的数组
       const provinceArr = this.allData.map((item) => {
@@ -62,18 +94,74 @@ export default {
         xAxis: {
           data: provinceArr
         },
+        dataZoom: {
+          show: false,
+          startValue: this.startValue,
+          endValue: this.endValue
+        },
         series: [
           {
-            data: valueArr
+            data: valueArr,
+            itemStyle: {
+              color: arg => {
+                let targetColorArr = null
+                if (arg.value > 300) {
+                  targetColorArr = colorArr[0]
+                } else if (arg.value > 200) {
+                  targetColorArr = colorArr[1]
+                } else {
+                  targetColorArr = colorArr[2]
+                }
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: targetColorArr[0]
+                  },
+                  {
+                    offset: 1,
+                    color: targetColorArr[1]
+                  }
+                ])
+              }
+            }
           }
         ]
       }
       this.chartInstance.setOption(dataOption)
     },
     screenAdapter() {
-      const adapterOption = {}
+      const titleFontSize = this.$refs.rank_ref.offsetWidth / 100 * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0]
+            }
+          }
+        ]
+      }
       this.chartInstance.setOption(adapterOption)
       this.chartInstance.resize()
+    },
+    startInterval () {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.startValue++
+        this.endValue++
+        if (this.endValue > this.allData.length) {
+          this.startValue = 0
+          this.endValue = 9
+        }
+        this.updateChart()
+      }, 2000)
     }
   }
 }
