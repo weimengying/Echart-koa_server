585fa4c072132f606b0847aa056ddc33eff708ea
[weimengying] <[weimengying515@163.com]>
2020-09-22 23:55:55 +0800

    库存和销量

---
+++ vision/src/components/Stock.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="com-container">
+    Stock组件
+    <div class="com-chart" ref="stock_ref"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null
+    }
+  },
+  mounted (){
+      this.initChart()
+      this.getData()
+      window.addEventListener('resize',)
+  },
+  methods: {
+    initChart() {
+      this.echartInstance = this.$echarts.init(this.$refs.stock_ref)
+      const initOption = {}
+      this.echartInstance.setOption(initOption)
+    }
+  }
+}
+</script>
+<style>
+</style>
